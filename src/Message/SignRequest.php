5e2de23a53d88804ade6bf78e5865fbccb258d75
Gregorio Hernandez Caso <gregoriohc@gmail.com>
2016-01-04 17:04:47 +0000

    Minor phpdoc fix

--- src/Message/SignRequest.php
+++ src/Message/SignRequest.php
@@ -16,7 +16,7 @@ class SignRequest extends AbstractRequest
     /**
      * Get document to sign
      *
-     * @return \Argentum\Common\Document\AbstractDocument
+     * @return \Argentum\Common\Document\AbstractDocument|\Argentum\Common\Document\Ticket
      */
     public function getDocument()
     {

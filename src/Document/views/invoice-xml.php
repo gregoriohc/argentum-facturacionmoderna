dcd0e5401c434a6f49fa239aa5a0ef5dc4f7bc50
Gregorio Hernández Caso <gregoriohc@gmail.com>
2016-03-02 10:23:29 +0000

    Add required parameters for MX digital invoice

--- src/Document/views/invoice-xml.php
+++ src/Document/views/invoice-xml.php
@@ -1,4 +1,7 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+/** @var \Argentum\FacturacionModerna\Document\Invoice $invoice */
+?>
 <cfdi:Comprobante
     xmlns:cfdi="http://www.sat.gob.mx/cfd/3"
     xmlns:xs="http://www.w3.org/2001/XMLSchema"
@@ -10,34 +13,72 @@
     noCertificado=""
     certificado=""
     sello=""
-    formaDePago="Pago en una sola exhibición"
-    metodoDePago="Transferencia Electrónica"
+    formaDePago="<?php echo $invoice->getPaymentType(); ?>"
+    metodoDePago="<?php echo $invoice->getPaymentMethod(); ?>"
     LugarExpedicion="<?php echo $invoice->getFrom()->getAddress(); ?>"
     subTotal="<?php echo round($invoice->getSubtotal(), 2); ?>"
     total="<?php echo round($invoice->getTotal(), 2); ?>"
     serie="A"
     folio="<?php echo $invoice->getId(); ?>"
-    condicionesDePago="Contado"
-    NumCtaPago="No identificado"
-    descuento="0.00"
+    condicionesDePago="<?php echo $invoice->getPaymentConditions(); ?>"
+    NumCtaPago="<?php echo $invoice->getPaymentAccount(); ?>"
+    descuento="<?php echo $invoice->getDiscount(); ?>"
     Moneda="<?php echo $invoice->getCurrency(); ?>"
     >
-    <cfdi:Emisor nombre="<?php echo $invoice->getFrom()->getName(); ?>" rfc="<?php echo $invoice->getFrom()->getId(); ?>">
-        <cfdi:DomicilioFiscal calle="RIO GUADALQUIVIR" noExterior="238" colonia="ORIENTE DEL VALLE" municipio="San Pedro Garza García" estado="Nuevo León" pais="México" codigoPostal="66220" />
-        <cfdi:RegimenFiscal Regimen="REGIMEN GENERAL DE LEY PERSONAS MORALES" />
+    <cfdi:Emisor
+        nombre="<?php echo $invoice->getFrom()->getName(); ?>"
+        rfc="<?php echo $invoice->getFrom()->getId(); ?>"
+    >
+        <cfdi:DomicilioFiscal
+            calle="<?php echo $invoice->getFrom()->getAddress()->getAddress_1(); ?>"
+            noExterior="<?php echo $invoice->getFrom()->getAddress()->getAddress_2(); ?>"
+            noInterior="<?php echo $invoice->getFrom()->getAddress()->getAddress_3(); ?>"
+            colonia="<?php echo $invoice->getFrom()->getAddress()->getNeighborhood(); ?>"
+            municipio="<?php echo $invoice->getFrom()->getAddress()->getLocality(); ?>"
+            estado="<?php echo $invoice->getFrom()->getAddress()->getState(); ?>"
+            pais="<?php echo $invoice->getFrom()->getAddress()->getCountry(); ?>"
+            codigoPostal="<?php echo $invoice->getFrom()->getAddress()->getPostcode(); ?>"
+        />
+        <cfdi:RegimenFiscal
+            Regimen="<?php echo $invoice->getScheme(); ?>"
+        />
     </cfdi:Emisor>
-    <cfdi:Receptor nombre="<?php echo $invoice->getTo()->getName(); ?>" rfc="<?php echo $invoice->getTo()->getId(); ?>">
-        <cfdi:Domicilio calle="CERRADA DE AZUCENAS" noExterior="109" colonia="REFORMA" municipio="Oaxaca de Juárez" estado="Oaxaca" pais="México" codigoPostal="68050" />
+    <cfdi:Receptor
+        nombre="<?php echo $invoice->getTo()->getName(); ?>"
+        rfc="<?php echo $invoice->getTo()->getId(); ?>"
+    >
+        <cfdi:Domicilio
+            calle="<?php echo $invoice->getTo()->getAddress()->getAddress_1(); ?>"
+            noExterior="<?php echo $invoice->getTo()->getAddress()->getAddress_2(); ?>"
+            noInterior="<?php echo $invoice->getTo()->getAddress()->getAddress_3(); ?>"
+            colonia="<?php echo $invoice->getTo()->getAddress()->getNeighborhood(); ?>"
+            municipio="<?php echo $invoice->getTo()->getAddress()->getLocality(); ?>"
+            estado="<?php echo $invoice->getTo()->getAddress()->getState(); ?>"
+            pais="<?php echo $invoice->getTo()->getAddress()->getCountry(); ?>"
+            codigoPostal="<?php echo $invoice->getTo()->getAddress()->getPostcode(); ?>"
+        />
     </cfdi:Receptor>
     <cfdi:Conceptos>
         <?php foreach ($invoice->getItems() as $item) : ?>
-            <cfdi:Concepto cantidad="<?php echo round($item->getQuantity(), 2); ?>" unidad="<?php echo (!empty($item->getUnit()) ? $item->getUnit() : 'No aplica'); ?>" descripcion="<?php echo $item->getName(); ?>" valorUnitario="<?php echo round($item->getPrice(), 2); ?>" importe="<?php echo round($item->getQuantity() * $item->getPrice(), 2); ?>"></cfdi:Concepto>
+            <cfdi:Concepto
+                cantidad="<?php echo round($item->getQuantity(), 2); ?>"
+                unidad="<?php echo (!empty($item->getUnit()) ? $item->getUnit() : 'No aplica'); ?>"
+                descripcion="<?php echo $item->getName(); ?>"
+                valorUnitario="<?php echo round($item->getPrice(), 2); ?>"
+                importe="<?php echo round($item->getQuantity() * $item->getPrice(), 2); ?>"
+            ></cfdi:Concepto>
         <?php endforeach; ?>
     </cfdi:Conceptos>
-    <cfdi:Impuestos totalImpuestosTrasladados="1.60">
+    <cfdi:Impuestos
+        totalImpuestosTrasladados="<?php echo $invoice->getTaxesAmount(); ?>"
+    >
         <cfdi:Traslados>
             <?php foreach ($invoice->getTaxes() as $tax) : ?>
-                <cfdi:Traslado impuesto="<?php echo $tax->getName(); ?>" tasa="<?php echo round($tax->getRate(), 2); ?>" importe="<?php echo round($tax->getAmount($invoice->getSubtotal()), 2); ?>"></cfdi:Traslado>
+                <cfdi:Traslado
+                    impuesto="<?php echo $tax->getName(); ?>"
+                    tasa="<?php echo round($tax->getRate(), 2); ?>"
+                    importe="<?php echo round($tax->getAmount($invoice->getSubtotal()), 2); ?>"
+                ></cfdi:Traslado>
             <?php endforeach; ?>
         </cfdi:Traslados>
     </cfdi:Impuestos>
